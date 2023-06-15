48877350a9d76c73f4d62d32ab3f6d367bd60776
MuhammedNihalcp <nihalriju915@gmail.com>
2023-06-15 16:00:54 +0530

    first init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:sample_login/screens/login_screen/view/login_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ScreenLogin(),
+    );
+  }
+}
